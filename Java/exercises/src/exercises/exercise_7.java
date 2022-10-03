6f5b2e9e3a7a99026d43b2857ae6e74977a5e493
Jimmy Karlsson <preaker18@gmail.com>
2022-10-03 10:24:51 +0200

    syncS

---
+++ Java/exercises/src/exercises/exercise_7.java
@@ -0,0 +1,29 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class exercise_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner myscanner = new Scanner(System.in);
+
+		System.out.println("Give me a number between 0 and 10.");
+		int userInput = myscanner.nextInt();
+
+		if (userInput > 10 && userInput < 0) {
+			System.out.println("I said between 0 and 10! The number yo gave where: " + userInput);
+		}
+
+		else {
+			return;
+		}
+
+
+		myscanner.close();
+
+
+	}
+
+}
