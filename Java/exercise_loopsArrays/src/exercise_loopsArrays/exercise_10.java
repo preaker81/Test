eae48190c8e8a27f496e5f8a0facaf3a0a7de63e
Jimmy Karlsson <preaker18@gmail.com>
2022-10-04 22:02:37 +0200

    sync

--- Java/exercise_loopsArrays/src/exercise_loopsArrays/exercise_10.java
+++ Java/exercise_loopsArrays/src/exercise_loopsArrays/exercise_10.java
@@ -7,20 +7,19 @@ public class exercise_10 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String[] nameList = { "ida", "jimmy", "johan", "frida", "gunnar", "tommy", "jenny", "annika", "linn",
-				"arzije" };
-		int[] apperance = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		String[] name = { "ida", "jimmy", "johan", "frida", "gunnar", "tommy", "jenny", "annika", "linn", "arzije" };
+		int[] count = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 		for (int i = 0; i < 15; i++) {
-			int rnd = new Random().nextInt(nameList.length);
-			apperance[rnd] += 1;
-			System.out.println(nameList[rnd]);
+			int rnd = new Random().nextInt(name.length);
+			count[rnd] += 1;
+			System.out.println(name[rnd]);
 		}
 
-		System.out.println("------------------------------------------");
+		System.out.println("----------");
 
 		for (int j = 0; j < 10; j++) {
-			System.out.println(nameList[j] + " - " + apperance[j]);
+			System.out.println(name[j] + " - " + count[j]);
 		}
 
 	}
