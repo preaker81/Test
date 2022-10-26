42665c749ddf3138791b03142ca0614bc1c1b85c
Jimmy Karlsson <preaker18@gmail.com>
2022-10-27 00:24:31 +0200

    sync

--- Java/javaSlutproject/src/javaSlutproject/Date.java
+++ Java/javaSlutproject/src/javaSlutproject/Date.java
@@ -6,23 +6,24 @@ import java.time.temporal.WeekFields;
 import java.util.Locale;
 
 class Date {
-	
-	public static String getDate(int x) {
+
+	protected static String getDate(int x) {
 		LocalDate now = LocalDate.now();
-		
+
 		TemporalField fieldISO = WeekFields.of(Locale.FRANCE).dayOfWeek();
 		LocalDate date = now.with(fieldISO, x);
-		
+
 		String dateString = date.toString();
 		return dateString;
 	}
-	
-public static String getWeekDay(int x) {
-	LocalDate now = LocalDate.now();
-	TemporalField fieldISO = WeekFields.of(Locale.FRANCE).dayOfWeek();
-	LocalDate date = now.with(fieldISO, x);
-	String weekday = date.getDayOfWeek().toString();
-	return weekday;
-}
+
+	protected static String getWeekDay(int x) {
+		LocalDate now = LocalDate.now();
+		TemporalField fieldISO = WeekFields.of(Locale.FRANCE).dayOfWeek();
+		LocalDate date = now.with(fieldISO, x);
+
+		String weekday = date.getDayOfWeek().toString();
+		return weekday;
+	}
 
 }
--- Java/javaSlutproject/src/javaSlutproject/GUI.java
+++ Java/javaSlutproject/src/javaSlutproject/GUI.java
@@ -1,84 +1,123 @@
 package javaSlutproject;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.time.LocalDate;
-
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 class GUI {
 
-	static void createAndDisplay() {
+	protected static void createAndDisplay() {
 
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBackground(Color.white);
-		frame.setSize(800, 350);
+		frame.setSize(1200, 500);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setLayout(new GridLayout());
 
-		// For loop för paneler
-		int dayOfWeek = 1;
-		for (int i = 1; i <= 7; i++) {
+		addPanels(frame);
 
+		frame.setVisible(true);
+	}
+
+	private static void addPanels(JFrame frame) {
+		for (int i = 1; i <= 7; i++) {
+			Border blackline = BorderFactory.createLineBorder(Color.black);
 			JPanel panel = new JPanel();
 			panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 			panel.setBounds(0, 0, 100, 400);
+			panel.setBorder(blackline);
 
-			if (Date.getDate(dayOfWeek).equals(LocalDate.now().toString())) {
-				panel.setBackground(Color.gray);
+			if (Date.getDate(i).equals(LocalDate.now().toString())) {
+				panel.setBackground(new Color(180, 206, 179));
 			} else {
-				panel.setBackground(Color.white);
+				panel.setBackground(Color.lightGray);
 			}
 
 			addComponents(i, panel, frame);
 			frame.add(panel);
-			dayOfWeek++;
 		}
-		frame.setVisible(true);
 	}
 
 	private static void addComponents(int x, JPanel container, JFrame frame) {
-		// Creates components
 		JLabel labelWeekday = new JLabel(Date.getWeekDay(x));
+		labelWeekday.setAlignmentX(Component.CENTER_ALIGNMENT);
+
 		JLabel labelDate = new JLabel(Date.getDate(x));
+		labelDate.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-		JPanel notes = new JPanel();
-		notes.setBackground(Color.white);
+		JPanel notePanel = new JPanel();
+		notePanel.setBackground(Color.white);
 
-		JTextField textfield = new JTextField("Add an event", 40);
-		textfield.setPreferredSize(new Dimension(0, 40));
+		JTextField textfield = new JTextField("Add an event", 20);
+		textfield.setPreferredSize(new Dimension(0, 30));
 		textfield.setMaximumSize(textfield.getPreferredSize());
+		textfield.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-		JButton button = new JButton("Add");
+		JButton addButton = new JButton("Add a note");
+		addButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		addButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JLabel note = new JLabel("Event");
+				notePanel.add(note);
+				note.setText("<html><p style=\"width:100px\">" + textfield.getText() + "</p><br></html>");
+				textfield.setText("");
+			}
+		});
+
+		JButton clearButton = new JButton("Clear Notes");
+		clearButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		clearButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				notePanel.removeAll();
+				frame.repaint();
+			}
+		});
+
+		addPlaceholderText(textfield, "Add an event");
 
 		container.add(labelWeekday);
 		container.add(labelDate);
-		container.add(notes);
+		container.add(notePanel);
 		container.add(textfield);
-		container.add(button);
+		container.add(addButton);
+		container.add(clearButton);
+	}
 
-		ActionListener buttonListener = new ActionListener() {
+	private static void addPlaceholderText(JTextField textfield, String placeholder) {
+		textfield.setForeground(Color.GRAY);
+		textfield.addFocusListener(new FocusListener() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (textfield.getText().equals(placeholder)) {
+					textfield.setText("");
+					textfield.setForeground(Color.BLACK);
+				}
+			}
 
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				JLabel note = new JLabel("Event");
-				notes.add(note);
-				note.setText(textfield.getText());
-
-				frame.revalidate();
+			public void focusLost(FocusEvent e) {
+				if (textfield.getText().isEmpty()) {
+					textfield.setForeground(Color.GRAY);
+					textfield.setText(placeholder);
+				}
 			}
-		};
-		button.addActionListener(buttonListener);
+		});
 	}
 
 }
