e443a7d736b63f23a5a0ab228c9aa5bde809b648
Jimmy Karlsson <preaker18@gmail.com>
2022-11-26 12:26:32 +0100

    sync

---
+++ Java/humbug/src/humbug/Main.java
@@ -0,0 +1,25 @@
+package humbug;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 100; i++) {
+
+			if ((i % 3 == 0) && (i % 5 == 0)) {
+				System.out.println("Humbug");
+			}
+			else if (i % 3 == 0) {
+				System.out.println("Hum");
+			}
+			else if (i % 5 == 0) {
+				System.out.println("Bug");
+			}
+			else {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
