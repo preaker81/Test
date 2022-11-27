e647b8d7682d101632447bf5dc110d4b2621d2f4
Jimmy Karlsson <preaker18@gmail.com>
2022-11-27 21:34:27 +0100

    sync

--- Java/humbug/src/humbug/Main.java
+++ Java/humbug/src/humbug/Main.java
@@ -1,25 +1,40 @@
 package humbug;
 
+import java.util.Iterator;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		for (int i = 1; i <= 100; i++) {
+//
+//			if ((i % 3 == 0) && (i % 5 == 0)) {
+//				System.out.println("Humbug");
+//			}
+//			else if (i % 3 == 0) {
+//				System.out.println("Hum");
+//			}
+//			else if (i % 5 == 0) {
+//				System.out.println("Bug");
+//			}
+//			else {
+//				System.out.println(i);
+//			}
 
-			if ((i % 3 == 0) && (i % 5 == 0)) {
-				System.out.println("Humbug");
+			String output = "";
+
+			if (i % 3 == 0) {
+				output += "Fizz";
 			}
-			else if (i % 3 == 0) {
-				System.out.println("Hum");
+			if (i % 5 == 0) {
+				output += "Buzz";
 			}
-			else if (i % 5 == 0) {
-				System.out.println("Bug");
+			if (output == "") {
+				output = "" + i;
 			}
-			else {
-				System.out.println(i);
-			}
+
+			System.out.println(output);
+
 		}
-
 	}
-
 }
